9eb046989d6bf06add5483ccb61299cc3e0d11b5
Intummadee <kataroja1@gmail.com>
2024-11-28 16:52:07 +0700

    save

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,17 @@
 
 
 <template>
-  <v-app 
-    id="inspire" :style="{background: $vuetify.theme.themes.dark.background}"
-  >
+  <v-app>
+  <v-main>
+    <DashboardHome />
+  </v-main>
     <!-- app ระบุว่า <v-app-bar> นี้จะอยู่ในบริบทของ v-app ซึ่งเป็นโครงสร้างหลักของแอปพลิเคชัน -->
     <!-- dark ทำให้ข้อความและไอคอนในแถบใช้โหมดสีเข้ม (เหมาะกับพื้นหลังสีสว่าง) -->
      <!-- ถ้าเป็น light นางคาดหวังว่าตัว text จะเป็นสีดำ กรณี dark คือเป็นสีขาว มันคือการปรับธีม -->
     
-      <SideBar />
-      <SideBarRight />
-
-
+      
+      
+      
   </v-app>
 </template>
 
@@ -23,16 +23,17 @@
 // import DownloadTable from './components/DownloadTable.vue';
 // import UploadTable from './components/UploadTable.vue';
 // import chileSlot from './components/chileSlot.vue';
-import SideBar from './components/SideBar.vue';
-import SideBarRight from './components/SideBarRight.vue';
+
+import DashboardHome from './components/DashboardHome.vue';
 import { ref } from 'vue';
 
 const theme = ref('light')
 
 export default {
   components: {
-    SideBar,
-    SideBarRight
+    // SideBar,
+    // SideBarRight,
+    DashboardHome,
     // DownloadTable,
     // UploadTable
   },
