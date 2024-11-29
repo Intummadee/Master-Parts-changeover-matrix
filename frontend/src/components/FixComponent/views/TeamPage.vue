112f35c2b85d218dfa394a6920374d6afd957e9a
Intummadee <kataroja1@gmail.com>
2024-11-29 12:31:04 +0700

    Update TeamPage.vue

--- frontend/src/components/FixComponent/views/TeamPage.vue
+++ frontend/src/components/FixComponent/views/TeamPage.vue
@@ -1,92 +1,83 @@
 <template>
-    <div class="team">
-      <h1 class="subheading grey--text">Team</h1>
-      <v-container >
-        <v-layout row wrap>
-           <v-flex
-          sm6
-          xs12
-          md6
-          lg3
-        >
-         <v-card class="ma-3">
-      <v-list-item  >
-         <v-list-item-avatar
-          tile
-          class="mt-n7"
-        >
-        <v-sheet color="green" width="80" height="80" elevation="10">
-              <v-icon dark large>store</v-icon>
-        </v-sheet>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <div class="overline text-right">Article</div>
-          <v-list-item-title class="headline mb-1 text-right" >523614</v-list-item-title>
-          <div><v-divider></v-divider></div>
-        </v-list-item-content> 
-      </v-list-item>
-      <v-card-actions>
-        <v-icon text class="ma-2">person</v-icon>
-        <div class="overline">Iyad</div>
-      </v-card-actions>
-    </v-card>
+  <div class="team">
+    <h1 class="subheading grey--text">Team</h1>
+
+    <v-container >
+      <v-layout row wrap>
+        <v-flex sm6 xs12 md6 lg3>
+          <v-card class="ma-3">
+            <v-list-item>
+
+              <v-list-item-avatar tile class="mt-n7">
+                <v-sheet color="green" width="80" height="80" elevation="10">
+                  <v-icon dark large>store</v-icon>
+                </v-sheet>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <div class="overline text-right">Article</div>
+                <v-list-item-title class="headline mb-1 text-right" >523614</v-list-item-title>
+                <div><v-divider></v-divider></div>
+              </v-list-item-content> 
+            </v-list-item>
+            
+            <v-card-actions>
+              <v-icon text class="ma-2">person</v-icon>
+              <div class="overline">Iyad</div>
+            </v-card-actions>
+          </v-card>
         </v-flex>
-         <v-flex
-          sm6
-          xs12
-          md6
-          lg3
-        >
-         <v-card class="ma-3">
-      <v-list-item>
-         <v-list-item-avatar
-          tile
-          class="mt-n7"
-        >
-        <v-sheet color="#F44336" width="80" height="80" elevation="10">
-              <v-icon dark large>subscriptions</v-icon>
-        </v-sheet>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <div class="overline text-right">Abonné</div>
-          <v-list-item-title class="headline mb-1 text-right" >+700</v-list-item-title>
-          <div><v-divider></v-divider></div>
-        </v-list-item-content> 
-      </v-list-item>
-      <v-card-actions>
-        <v-icon text class="ma-2">subscriptions</v-icon>
-        <div class="overline">AAE IdeaPro</div>
-      </v-card-actions>
-    </v-card>
+
+
+        <v-flex sm6 xs12 md6 lg3>
+          <v-card class="ma-3">
+            <v-list-item>
+              <v-list-item-avatar tile class="mt-n7">
+                <v-sheet color="#F44336" width="80" height="80" elevation="10">
+                      <v-icon dark large>subscriptions</v-icon>
+                </v-sheet>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <div class="overline text-right">Abonné</div>
+                <v-list-item-title class="headline mb-1 text-right" >+700</v-list-item-title>
+                <div><v-divider></v-divider></div>
+              </v-list-item-content> 
+            </v-list-item>
+
+
+            <v-card-actions>
+              <v-icon text class="ma-2">subscriptions</v-icon>
+              <div class="overline">AAE IdeaPro</div>
+            </v-card-actions>
+          </v-card>
         </v-flex>
-         <v-flex
-          sm6
-          xs12
-          md6
-          lg3
-        >
-         <v-card class="ma-3">
-      <v-list-item>
-         <v-list-item-avatar
-          tile
-          class="mt-n7"
-        >
-        <v-sheet color="#03A9F4" width="80" height="80" elevation="10">
-              <v-icon dark large>add_shopping_cart</v-icon>
-        </v-sheet>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <div class="overline text-right">Shopping</div>
-          <v-list-item-title class="headline mb-1 text-right" >$34,245</v-list-item-title>
-          <div><v-divider></v-divider></div>
-        </v-list-item-content> 
-      </v-list-item>
-      <v-card-actions>
-        <v-icon text class="ma-2">credit_card</v-icon>
-        <div class="overline">VISA Card</div>
-      </v-card-actions>
-    </v-card>
+         
+        
+        <v-flex sm6 xs12 md6 lg3 >
+          <v-card class="ma-3">
+            <v-list-item>
+              <v-list-item-avatar tile class="mt-n7" >
+                <v-sheet color="#03A9F4" width="80" height="80" elevation="10">
+                      <v-icon dark large>add_shopping_cart</v-icon>
+                </v-sheet>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <div class="overline text-right">Shopping</div>
+                <v-list-item-title class="headline mb-1 text-right" >$34,245</v-list-item-title>
+                <div><v-divider></v-divider></div>
+              </v-list-item-content> 
+            </v-list-item>
+
+            <v-card-actions>
+              <v-icon text class="ma-2">credit_card</v-icon>
+              <div class="overline">VISA Card</div>
+            </v-card-actions>
+          </v-card>
         </v-flex>
+
+        
          <v-flex
           sm6
           xs12
