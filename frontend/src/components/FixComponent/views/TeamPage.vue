1b438045b6d74fae94d32de2ed5209414df41b99
Intummadee <kataroja1@gmail.com>
2024-11-29 12:32:53 +0700

    Update TeamPage.vue

--- frontend/src/components/FixComponent/views/TeamPage.vue
+++ frontend/src/components/FixComponent/views/TeamPage.vue
@@ -77,54 +77,55 @@
           </v-card>
         </v-flex>
 
-        
-         <v-flex
-          sm6
-          xs12
-          md6
-          lg3
-        >
-         <v-card class="ma-3">
-      <v-list-item >
-         <v-list-item-avatar
-          tile
-          class="mt-n7"
-        >
-        <v-sheet color="#FFC107" width="80" height="80" elevation="10">
-              <v-icon dark large>folder_shared</v-icon>
-        </v-sheet>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <div class="overline text-right">Folder shared</div>
-          <v-list-item-title class="headline mb-1 text-right" >1730</v-list-item-title>
-          <div><v-divider></v-divider></div>
-        </v-list-item-content> 
-      </v-list-item>
-      <v-card-actions>
-        <v-icon text class="ma-2">folder</v-icon>
-        <div class="overline">Prodect</div>
-      </v-card-actions>
-    </v-card>
+
+        <v-flex sm6 xs12 md6 lg3 >
+          <v-card class="ma-3">
+            <v-list-item >
+              <v-list-item-avatar tile class="mt-n7" >
+                <v-sheet color="#FFC107" width="80" height="80" elevation="10">
+                  <v-icon dark large>folder_shared</v-icon>
+                </v-sheet>
+              </v-list-item-avatar>
+
+
+              <v-list-item-content>
+                <div class="overline text-right">Folder shared</div>
+                <v-list-item-title class="headline mb-1 text-right" >1730</v-list-item-title>
+                <div><v-divider></v-divider></div>
+              </v-list-item-content> 
+            </v-list-item>
+
+
+            <v-card-actions>
+              <v-icon text class="ma-2">folder</v-icon>
+              <div class="overline">Prodect</div>
+            </v-card-actions>
+
+          </v-card>
+
         </v-flex>
-          <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
-                <v-card class="text-center ma-3">
-                  <v-responsive class="pt-4">
-                     <v-avatar size="100" class="red lighten-2">
-                       <img :src="person.avatar" alt="" >
-                     </v-avatar>
-                  </v-responsive>
-                  <v-card-text>
-                    <div class="subheading">{{person.name}}</div>
-                    <div class="grey--text">{{person.role}}</div>
-                  </v-card-text>
-                  <v-card-actions>
-                      <v-btn outlined color="orange">
-                          <v-icon small left >message</v-icon>
-                          <span>Message</span>
-                      </v-btn>
-                  </v-card-actions>
-                </v-card>
-          </v-flex>
+
+
+        <!-- Person in team -->
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+              <v-card class="text-center ma-3">
+                <v-responsive class="pt-4">
+                    <v-avatar size="100" class="red lighten-2">
+                      <img :src="person.avatar" alt="" >
+                    </v-avatar>
+                </v-responsive>
+                <v-card-text>
+                  <div class="subheading">{{person.name}}</div>
+                  <div class="grey--text">{{person.role}}</div>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn outlined color="orange">
+                        <v-icon small left >message</v-icon>
+                        <span>Message</span>
+                    </v-btn>
+                </v-card-actions>
+              </v-card>
+        </v-flex>
          
         </v-layout>
       </v-container>
